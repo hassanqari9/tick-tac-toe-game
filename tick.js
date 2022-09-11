a5bde0071c0b96921938e5a6fdc8cd6ae5bb04ee
Hasaan Qari <76206623+hassanqari9@users.noreply.github.com>
2022-09-11 17:39:48 +0530

    Update tick.js

--- tick.js
+++ tick.js
@@ -10,8 +10,8 @@ document.querySelectorAll("button")[i].addEventListener("click",function(){
             document.querySelector("."+classes+"").innerHTML="x";
             var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
-            document.querySelector("h2").innerHTML="Player 2 turn:";
-            document.querySelector("h2").style.color="#00D7FF";
+//             document.querySelector("h2").innerHTML="Player 2 turn:";
+//             document.querySelector("h2").style.color="#00D7FF";
             break;
         }
         else if (j%2==0)
@@ -19,8 +19,8 @@ document.querySelectorAll("button")[i].addEventListener("click",function(){
             document.querySelector("."+classes+"").innerHTML="o";
             var snare = new Audio("sounds/snare.mp3");
             snare.play();
-            document.querySelector("h2").innerHTML="Player 1 turn:";
-            document.querySelector("h2").style.color="#7DCE13";
+//             document.querySelector("h2").innerHTML="Player 1 turn:";
+//             document.querySelector("h2").style.color="#7DCE13";
             break;
         }
     }
