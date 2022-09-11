300e1825966ef731566580b0eef628e07632795f
Hasaan Qari <76206623+hassanqari9@users.noreply.github.com>
2022-09-11 17:44:50 +0530

    Update tick.js

--- tick.js
+++ tick.js
@@ -8,6 +8,8 @@ document.querySelectorAll("button")[i].addEventListener("click",function(){
         
         if (j%2!=0){
             document.querySelector("."+classes+"").innerHTML="x";
+            document.querySelector("."+classes+"").style.color="#00D7FF";
+            
             var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
 //             document.querySelector("h2").innerHTML="Player 2 turn:";
@@ -17,6 +19,7 @@ document.querySelectorAll("button")[i].addEventListener("click",function(){
         else if (j%2==0)
         {
             document.querySelector("."+classes+"").innerHTML="o";
+            document.querySelector("."+classes+"").style.color="#7DCE13";
             var snare = new Audio("sounds/snare.mp3");
             snare.play();
 //             document.querySelector("h2").innerHTML="Player 1 turn:";
